ecb840f7fe48ff256a50604fdfcb96f0e282a7f5
Mats Eikeland Mollestad <mem@mollestad.no>
2019-05-02 21:35:47 +0200

    Added better support for optional date formating

--- Sources/HTMLKit/DateVariable.swift
+++ Sources/HTMLKit/DateVariable.swift
@@ -10,6 +10,11 @@ import Foundation
 /// A struct that renders a data in a specified formate
 struct DateVariable<T: ContextualTemplate>: CompiledTemplate {
 
+    enum Referance {
+        case optional(KeyPath<T.Context, Date?>)
+        case solid(KeyPath<T.Context, Date>)
+    }
+
     enum Errors: LocalizedError {
         case unableToCopyFormatter
 
@@ -22,13 +27,21 @@ struct DateVariable<T: ContextualTemplate>: CompiledTemplate {
     let formatter: DateFormatter
 
     /// The key path to the date to render
-    let datePath: KeyPath<T.Context, Date>
+    let dateReferance: Referance
 
 
     // View `CompiledTemplate`
     func render<T>(with manager: HTMLRenderer.ContextManager<T>) throws -> String {
 
-        let date = try manager.value(at: datePath)
+        var optionalDate: Date?
+        switch dateReferance {
+        case .optional(let keyPath): optionalDate = try manager.value(at: keyPath)
+        case .solid(let keyPath): optionalDate = try manager.value(at: keyPath)
+        }
+
+        guard let date = optionalDate else {
+            return ""
+        }
 
         if let locale = manager.locale {
 
@@ -63,7 +76,7 @@ extension ContextualTemplate {
         let formatter = DateFormatter()
         formatter.dateStyle = dateStyle
         formatter.timeStyle = timeStyle
-        return DateVariable<Self>(formatter: formatter, datePath: datePath)
+        return DateVariable<Self>(formatter: formatter, dateReferance: .solid(datePath))
     }
 
     /// Render a date in a formate
@@ -75,6 +88,32 @@ extension ContextualTemplate {
     public func date(_ datePath: KeyPath<Context, Date>, format: String) -> CompiledTemplate {
         let formatter = DateFormatter()
         formatter.dateFormat = format
-        return DateVariable<Self>(formatter: formatter, datePath: datePath)
+        return DateVariable<Self>(formatter: formatter, dateReferance: .solid(datePath))
+    }
+
+    /// Render a date in a formate
+    ///
+    /// - Parameters:
+    ///   - datePath: The path to the date
+    ///   - dateStyle: The style to use to render the date
+    ///   - timeStyle: The style to use to render the time
+    /// - Returns: A `CompiledTemplate`
+    public func date(_ datePath: KeyPath<Context, Date?>, dateStyle: DateFormatter.Style, timeStyle: DateFormatter.Style) -> CompiledTemplate {
+        let formatter = DateFormatter()
+        formatter.dateStyle = dateStyle
+        formatter.timeStyle = timeStyle
+        return DateVariable<Self>(formatter: formatter, dateReferance: .optional(datePath))
+    }
+
+    /// Render a date in a formate
+    ///
+    /// - Parameters:
+    ///   - datePath: The path to the date
+    ///   - dateFormat: The formate to render the date with
+    /// - Returns: A `CompiledTemplate`
+    public func date(_ datePath: KeyPath<Context, Date?>, format: String) -> CompiledTemplate {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return DateVariable<Self>(formatter: formatter, dateReferance: .optional(datePath))
     }
 }
