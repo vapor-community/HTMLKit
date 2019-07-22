f3a336057e526eac7601c49ff4589c06f3daf84c
Mats E. Mollestad <mats@mollestad.no>
2019-07-22 16:08:39 +0200

    fix: removed dependencies from Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HTMLKit",
-            dependencies: ["Lingo", "SwiftMarkdown"]),
+            dependencies: []),
         .testTarget(
             name: "HTMLKitTests",
             dependencies: ["HTMLKit"]),
