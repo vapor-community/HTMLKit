5d05af120c8fd7538207782b3c9792418a5cbf63
Mats Eikeland Mollestad <mem@mollestad.no>
2019-10-21 22:15:53 +0200

    Vapor 3 support

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
 
         .package(url: "https://github.com/miroslavkovac/Lingo.git", from: "3.0.5"),
 //        .package(url: "https://github.com/vapor-community/markdown.git", .upToNextMajor(from: "0.4.0")),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-alpha.3"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/HTMLKitVapor/HTMLRenderable+Vapor.swift
+++ Sources/HTMLKitVapor/HTMLRenderable+Vapor.swift
@@ -5,8 +5,8 @@
 ////  Created by Mats Mollestad on 16/09/2019.
 ////
 //
-//import Vapor
-//import HTMLKit
+import Vapor
+import HTMLKit
 //
 //
 /////// An extension that implements most of the helper functions
@@ -27,3 +27,34 @@
 //        return try Response(headers: .init([("content-type", "text/html; charset=utf-8")]), body: .init(string: renderRaw(type)))
 //    }
 //}
+
+extension HTMLRenderable {
+
+    /// Renders a `StaticView` formula
+    ///
+    ///     try renderer.render(WelcomeView.self)
+    ///
+    /// - Parameter type: The view type to render
+    /// - Returns: Returns a rendered view in a `Response`
+    /// - Throws: If the formula do not exists, or if the rendering process fails
+    public func render<T: TemplateView>(_ type: T.Type, with value: T.Value) throws -> HTTPResponse {
+        try HTTPResponse(headers: .init([("content-type", "text/html; charset=utf-8")]), body: renderRaw(type, with: value))
+    }
+
+    public func render<T: StaticView>(_ type: T.Type) throws -> HTTPResponse {
+        try HTTPResponse(headers: .init([("content-type", "text/html; charset=utf-8")]), body: renderRaw(type))
+    }
+}
+
+extension HTMLRenderer: Service {}
+
+extension Request {
+
+    /// Creates a `HTMLRenderer` that can render templates
+    ///
+    /// - Returns: A `HTMLRenderer` containing all the templates
+    /// - Throws: If the shared container could not make the `HTMLRenderer`
+    public func renderer() throws -> HTMLRenderable {
+        return try sharedContainer.make(HTMLRenderable.self)
+    }
+}
