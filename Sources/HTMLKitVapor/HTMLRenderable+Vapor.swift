ee1c5efda1b719463c7014ed953e7173dcef706c
Mats Eikeland Mollestad <mem@mollestad.no>
2019-10-29 15:34:10 +0100

    HTMLKitVapor fix for Vapor 3

--- Sources/HTMLKitVapor/HTMLRenderable+Vapor.swift
+++ Sources/HTMLKitVapor/HTMLRenderable+Vapor.swift
@@ -8,18 +8,38 @@ import HTMLKit
 ///// An extension that implements most of the helper functions
 extension HTMLRenderable {
 
+    /// Renders a `TemplateView` formula
+    ///
+    ///     try renderer.render(WelcomeView.self)
+    ///
+    /// - Parameters:
+    ///   - type: The view type to render
+    ///   - context: The needed context to render the view with
+    /// - Returns: Returns a rendered view in a `HTTPResponse`
+    /// - Throws: If the formula do not exists, or if the rendering process fails
+    public func render<T: TemplateView>(_ type: T.Type, with context: T.Value) throws -> HTTPResponse {
+        return try HTTPResponse(headers: .init([("content-type", "text/html; charset=utf-8")]), body: renderRaw(type, with: context))
+    }
+
     /// Renders a `StaticView` formula
     ///
     ///     try renderer.render(WelcomeView.self)
     ///
     /// - Parameter type: The view type to render
-    /// - Returns: Returns a rendered view in a `Response`
+    /// - Returns: Returns a rendered view in a `HTTPResponse`
     /// - Throws: If the formula do not exists, or if the rendering process fails
-    public func render<T: TemplateView>(_ type: T.Type, with value: T.Value) throws -> Response {
-        return try Response(headers: .init([("content-type", "text/html; charset=utf-8")]), body: .init(string: renderRaw(type, with: value)))
+    public func render<T>(_ type: T.Type) throws -> HTTPResponse where T : StaticView {
+        return try HTTPResponse(headers: .init([("content-type", "text/html; charset=utf-8")]), body: renderRaw(type))
     }
+}
 
-    public func render<T: StaticView>(_ type: T.Type) throws -> Response {
-        return try Response(headers: .init([("content-type", "text/html; charset=utf-8")]), body: .init(string: renderRaw(type)))
+extension Request {
+
+    /// Creates a `HTMLRenderer` that can render templates
+    ///
+    /// - Returns: A `HTMLRenderer` containing all the templates
+    /// - Throws: If the shared container could not make the `HTMLRenderer`
+    public func renderer() throws -> HTMLRenderer {
+        return try sharedContainer.make(HTMLRenderer.self)
     }
 }
