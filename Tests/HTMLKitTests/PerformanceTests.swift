15c9499ed54f065c3351b2746e8b233b30efac75
Mattes Mohr <mail@mattesmohr.de>
2021-11-01 15:59:01 +0100

    Add some tests

--- Tests/HTMLKitTests/PerformanceTests.swift
+++ Tests/HTMLKitTests/PerformanceTests.swift
@@ -73,13 +73,80 @@ final class PerformanceTests: XCTestCase {
             _ = try! renderer.render(raw: TestPage.self)
         }
     }
+    
+    func testPerformanceWithAttributes() throws {
+        
+        let view = TestPage {
+            Html {
+                Body {
+                    Header {
+                        Heading1 {
+                            "Performance Test"
+                        }
+                        .id("test")
+                        .class("class")
+                        .role("role")
+                    }
+                    Main {
+                        Article {
+                            Heading3 {
+                                "Test"
+                            }
+                            .id("test")
+                            .class("class")
+                            .role("role")
+                        }
+                    }
+                    .id("test")
+                    .class("class")
+                    .role("role")
+                }
+            }
+        }
+        
+        try renderer.add(view: view)
+        
+        measure {
+            _ = try! renderer.render(raw: TestPage.self)
+        }
+    }
+    
+    func testPerformanceWithoutAttributes() throws {
+        
+        let view = TestPage {
+            Html {
+                Body {
+                    Header {
+                        Heading1 {
+                            "Performance Test"
+                        }
+                    }
+                    Main {
+                        Article {
+                            Heading3 {
+                                "Test"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        try renderer.add(view: view)
+        
+        measure {
+            _ = try! renderer.render(raw: TestPage.self)
+        }
+    }
 }
 
 extension PerformanceTests {
     
     static var allTests = [
         ("testPerformanceRenderingWhenUsingDefault", testPerformanceRenderingWhenUsingDefault),
-        ("testPerformanceRenderingWhenUsingTypealiases", testPerformanceRenderingWhenUsingTypealiases)
+        ("testPerformanceRenderingWhenUsingTypealiases", testPerformanceRenderingWhenUsingTypealiases),
+        ("testPerformanceWithAttributes", testPerformanceWithAttributes),
+        ("testPerformanceWithoutAttributes", testPerformanceWithoutAttributes)
     ]
 }
 
