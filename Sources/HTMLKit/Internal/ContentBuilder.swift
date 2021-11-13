669897c5e9cb986585e26baa98e9a35b272c5bd4
Mattes Mohr <mail@mattesmohr.de>
2021-11-13 22:44:29 +0100

    Extend the result builder

--- Sources/HTMLKit/Internal/ContentBuilder.swift
+++ Sources/HTMLKit/Internal/ContentBuilder.swift
@@ -6,4 +6,20 @@
     public static func buildBlock(_ children: Content...) -> Content {
         return children
     }
+    
+    public static func buildExpression(_ expression: Content) -> Content {
+       return expression
+     }
+    
+    public static func buildEither(first component: Content) -> Content {
+        component
+    }
+
+    public static func buildEither(second component: Content) -> Content {
+        component
+    }
+    
+    public static func buildOptional(_ component: Content?) -> Content {
+        return component ?? []
+    }
 }
