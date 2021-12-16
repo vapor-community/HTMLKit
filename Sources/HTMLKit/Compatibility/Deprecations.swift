fd6a0beb3ac81d2956f77c00a0f0b931675381a4
Mattes Mohr <mail@mattesmohr.de>
2021-12-16 20:41:27 +0100

    Add deprecations

---
+++ Sources/HTMLKit/Compatibility/Deprecations.swift
@@ -0,0 +1,32 @@
+@available(*, deprecated, renamed: "Renderer")
+public class HTMLRenderer {}
+
+@available(*, deprecated, renamed: "ContentBuilder")
+public class HTMLBuilder {}
+
+@available(*, deprecated, renamed: "AnyContent")
+public protocol HTML {}
+
+@available(*, deprecated, renamed: "AnyContent")
+public protocol HTMLContent {}
+
+@available(*, deprecated, renamed: "AnyContent")
+public protocol Content {}
+
+@available(*, deprecated, renamed: "Page")
+public protocol HTMLTemplate {}
+
+@available(*, deprecated, renamed: "View")
+public protocol HTMLPage {}
+
+@available(*, deprecated, renamed: "Component")
+public protocol HTMLComponent {}
+
+@available(*, deprecated, message: "Is no longer in the scope.")
+public struct HTMLAttribute {}
+
+@available(*, deprecated, renamed: "AnyNode")
+public protocol HTMLNode {}
+
+@available(*, deprecated, message: "Is no longer in the scope.")
+public enum HTMLIdentifier {}
