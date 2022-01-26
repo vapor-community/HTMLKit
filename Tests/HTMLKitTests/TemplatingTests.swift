8aa0191e5f748b20c6e36c5cf9454480144d664f
Mattes Mohr <mail@mattesmohr.de>
2022-01-26 20:31:57 +0100

    Fix minor issue

--- Tests/HTMLKitTests/TemplatingTests.swift
+++ Tests/HTMLKitTests/TemplatingTests.swift
@@ -179,6 +179,8 @@ final class TemplatingTests: XCTestCase {
                        <body>\
                        <h1>Hello World!</h1>\
                        <h2>Hello World!</h2>\
+                       <script></script>\
+                       <script></script>\
                        </body>\
                        </html>
                        """
