acfc7a806af01027de8c9629781458dfacaad8e2
Kyle <kyle201817146@gmail.com>
2022-03-13 21:46:04 +0800

    Enable support for..in loops in resultBuilder block

--- Sources/HTMLKit/Internal/Core/Builders/ContentBuilder.swift
+++ Sources/HTMLKit/Internal/Core/Builders/ContentBuilder.swift
@@ -14,40 +14,11 @@
 /// ## References
 ///
 @resultBuilder public class ContentBuilder<T> {
-    
-    public static func buildBlock() -> [T] {
-        return []
-    }
-    
-    public static func buildBlock(_ component: T) -> [T] {
-        return [component]
-    }
-
     public static func buildBlock(_ components: T...) -> [T] {
-        return components
+        components
     }
     
-    public static func buildBlock(_ components: [T], _ trailing: T...) -> [T] {
-        return components + trailing
-    }
-    
-    public static func buildBlock(_ c1: T, _ components: [T], _ trailing: T...) -> [T] {
-        return [c1] + components + trailing
-    }
-    
-    public static func buildBlock(_ c1: T, _ c2: T, _ components: [T], _ trailing: T...) -> [T] {
-        return [c1, c2] + components + trailing
-    }
-    
-    public static func buildBlock(_ c1: T, _ c2: T, _ c3: T, _ components: [T], _ trailing: T...) -> [T] {
-        return [c1, c2, c3] + components + trailing
-    }
-    
-    public static func buildBlock(_ cs1: [T], _ components: [T], _ trailing: T...) -> [T] {
-        return cs1 + components + trailing
-    }
-    
-    public static func buildBlock(_ cs1: [T], _ cs2: [T], _ components: [T], _ trailing: T...) -> [T] {
-        return cs1 + cs2 + components + trailing
+    public static func buildArray(_ components: [[T]]) -> [T] {
+        components.flatMap { $0 }
     }
 }
