946737564b601a0432fe3cd407ffbb859bf12790
Mattes Mohr <mail@mattesmohr.de>
2022-11-12 19:14:34 +0100

    Refactor the formula class
    The formula should be only used by the renderer. Therefore the access level should be internal.

--- Sources/HTMLKit/Framework/Rendering/Formula.swift
+++ Sources/HTMLKit/Framework/Rendering/Formula.swift
@@ -4,21 +4,20 @@
  */
 
 import Foundation
-import Lingo
 
-public class Formula {
+internal class Formula {
 
     /// The different ingredients of the content.
-    public var ingredients: [AnyContent]
+    internal var ingredients: [AnyContent]
 
     /// The calender for localization.
-    public var calendar: Calendar
+    internal var calendar: Calendar
 
     /// The time zone for localization.
-    public var timeZone: TimeZone
+    internal var timeZone: TimeZone
 
     /// Initiates a formula.
-    public init(calendar: Calendar = .current, timeZone: TimeZone = .current) {
+    internal init(calendar: Calendar = .current, timeZone: TimeZone = .current) {
         
         self.ingredients = []
         self.calendar = calendar
@@ -26,18 +25,7 @@ public class Formula {
     }
 
     /// Appends the ingredients
-    public func add<T: AnyContent>(content: T) {
-        self.ingredients.append(content)
-    }
-
-    public func render<T>(with context: T, lingo: Lingo?) throws -> String {
-        return try render(with: ContextManager(context: context, lingo: lingo))
-    }
-
-    public func render<T>(with manager: ContextManager<T>) throws -> String {
-        
-        return try ingredients.reduce(into: "") { (result, ingridient) in
-            return result += try ingridient.render(with: manager)
-        }
+    internal func add(ingridient: some AnyContent) {
+        self.ingredients.append(ingridient)
     }
 }
