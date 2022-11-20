ccb1707b6dba1eba04dccc46a240aa630eac4faa
Mattes Mohr <mail@mattesmohr.de>
2022-11-20 10:10:48 +0100

    Revise the rendering for the localized

--- Sources/HTMLKit/Framework/Environment/Localization/Localized.swift
+++ Sources/HTMLKit/Framework/Environment/Localization/Localized.swift
@@ -32,3 +32,37 @@ extension Localized where B == NoData {
         self.context = nil
     }
 }
+
+extension Localized: Node {
+    
+    internal func prerender(with formula: Formula) {
+        formula.add(ingridient: self)
+    }
+    
+    internal func render<T>(with manager: ContextManager<T>) -> String {
+        
+        var result = ""
+        
+        if let lingo = manager.lingo {
+            
+            let locale = manager.locale ?? lingo.defaultLocale
+            
+            if let context = self.context {
+
+                if let value = try? context.value(from: manager) {
+                    
+                    if let data = try? JSONEncoder().encode(value) {
+
+                        let dictionary = try? JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed) as? [String: Any]
+                        
+                        result += lingo.localize(self.key, locale: locale, interpolations: dictionary)
+                    }
+                }
+            } else {
+                result += lingo.localize(self.key, locale: locale)
+            }
+        }
+        
+        return result
+    }
+}
