2ab2d83610d36e5fccc02957833cb2039177183d
Mattes Mohr <mail@mattesmohr.de>
2023-02-04 14:21:25 +0100

    Extend the carousel component

--- Sources/HTMLKitComponents/Components/Carousel.swift
+++ Sources/HTMLKitComponents/Components/Carousel.swift
@@ -7,15 +7,25 @@ import HTMLKit
 
 public struct Carousel: View {
     
-    public var indication: [Content]
+    internal var indication: [Content]
     
-    public var content: [Content]
+    internal var content: [Content]
+    
+    internal var classes: [String]
     
     public init(@ContentBuilder<Content> content: () -> [Content],
                 @ContentBuilder<Content> indication: () -> [Content]) {
         
         self.content = content()
         self.indication = indication()
+        self.classes = ["carousel"]
+    }
+    
+    internal init(indication: [Content], content: [Content], classes: [String]) {
+        
+        self.indication = indication
+        self.content = content
+        self.classes = classes
     }
     
     public var body: Content {
@@ -29,22 +39,75 @@ public struct Carousel: View {
             }
             .class("carousel-indication")
         }
-        .class("carousel")
+        .class(classes.joined(separator: " "))
+    }
+}
+
+public struct Slide: View, Identifiable, Modifiable {
+    
+    internal var id: String?
+    
+    internal var source: String
+    
+    internal var classes: [String]
+    
+    internal var caption: [Content]
+    
+    public init(source: String, @ContentBuilder<Content> caption: () -> [Content]) {
+        
+        self.source = source
+        self.caption = caption()
+        self.classes = ["slide"]
+    }
+    
+    internal init(id: String?, source: String, caption: [Content], classes: [String]) {
+        
+        self.id = id
+        self.source = source
+        self.caption = caption
+        self.classes = classes
+    }
+    
+    public var body: Content {
+        Division {
+            Division {
+                HTMLKit.Image()
+                    .source(source)
+            }
+            .class("slide-thumbnail")
+            Division {
+                caption
+            }
+            .class("slide-caption")
+        }
+        .class(classes.joined(separator: " "))
+        .modify(unwrap: id) {
+            $0.id($1)
+        }
+    }
+    
+    public func tag(_ value: String) -> Slide {
+        return self.mutate(id: value)
     }
 }
 
 public struct Indicator: View {
     
-    public var id: String
+    internal var tag: String
     
-    public init(id: String) {
-        self.id = id
+    public init(for tag: String) {
+        self.tag = "#" + tag
+    }
+    
+    internal init(tag: String) {
+        self.tag = tag
     }
     
     public var body: Content {
         Anchor {
         }
-        .reference(id)
+        .reference(tag)
         .class("indicator")
     }
 }
+
